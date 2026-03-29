10dbf9b08bfebfa225653121e8f86b86a1e9b656
Anastasiia K <ana@Anastasiias-MacBook-Air.local>
2026-03-29 19:19:29 +0300

    Adding 00_init_database.sql ;

---
+++ scripts/00_init_database.sql
@@ -0,0 +1,87 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouseAnalytics' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, this script creates a schema called gold
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouseAnalytics' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+
+-- 1. Terminate existing connections to the database
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'data_warehouse_analytics'
+  AND pid <> pg_backend_pid();
+
+-- 2. Delete db if exists
+DROP DATABASE IF EXISTS data_warehouse_analytics;
+
+-- 3. Creating db
+CREATE DATABASE data_warehouse_analytics;
+
+-- 4. Create Schema
+
+CREATE SCHEMA IF NOT EXISTS gold;
+
+DROP TABLE IF EXISTS gold.dim_customers;
+CREATE TABLE gold.dim_customers(
+	customer_key int,
+	customer_id int,
+	customer_number varchar(50),
+	first_name varchar(50),
+	last_name varchar(50),
+	country varchar(50),
+	marital_status varchar(50),
+	gender varchar(50),
+	birthdate date,
+	create_date date
+);
+
+DROP TABLE IF EXISTS gold.dim_products;
+CREATE TABLE gold.dim_products(
+	product_key int ,
+	product_id int ,
+	product_number varchar(50) ,
+	product_name varchar(50) ,
+	category_id varchar(50) ,
+	category varchar(50) ,
+	subcategory varchar(50) ,
+	maintenance varchar(50) ,
+	cost int,
+	product_line varchar(50),
+	start_date date 
+);
+
+DROP TABLE IF EXISTS gold.fact_sales;
+CREATE TABLE gold.fact_sales(
+	order_number varchar(50),
+	product_key int,
+	customer_key int,
+	order_date date,
+	shipping_date date,
+	due_date date,
+	sales_amount int,
+	quantity smallint,
+	price int 
+);
+
+
+
+TRUNCATE TABLE gold.dim_customers;
+COPY gold.dim_customers FROM '/datasets/source_crm/gold.dim_customers.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+
+
+
+TRUNCATE TABLE gold.dim_products;
+COPY gold.dim_products FROM '/datasets/source_crm/gold.dim_products.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+
+
+
+TRUNCATE TABLE gold.fact_sales;
+COPY gold.fact_sales FROM '/datasets/source_crm/gold.fact_sales.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
